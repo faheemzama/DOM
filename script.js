900f90d06c5006bbaea6eda9f85817dafab9193c
faheemzama <faheemzama786@gmail.com>
2021-08-21 09:25:15 +0530

    DOM

---
+++ script.js
@@ -0,0 +1,36 @@
+
+let parentE1 = document.getElementById("parent");
+console.log(parentE1);
+
+let parentE1 = document.getElementById("parent");
+console.log(parentE1.lastChild);
+
+let parentE1 = document.getElementById("parent");
+console.log(parentE1.children);
+
+let parentE1 = document.getElementById("parent");
+console.log(parentE1.childNodes);
+
+let parentE1 = document.getElementById("parent");
+console.log(parentE1.firstElementChild);
+
+let parentE1 = document.getElementById("parent");
+console.log(parentE1.children[2]);
+
+let midPE1 = document.getElementById("midparagraph");
+console.log(midPE1);
+
+let midPE1 = document.getElementById("midparagraph");
+console.log(midPE1.previousSibling);
+
+let midPE1 = document.getElementById("midparagraph");
+console.log(midPE1.nextSibling);
+
+let midPE1 = document.getElementById("midparagraph");
+console.log(midPE1.nextElementSibling);
+
+let midPE1 = document.getElementById("midparagraph");
+console.log(midPE1.previousElementSibling);
+
+let midPE1 = document.getElementById("midparagraph");
+console.log(midPE1.parentNode);
